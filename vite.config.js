16832f8dc6cea8c539997d69c8cb70086bdc0019
nerdnightboy <nerdnightboy@gmail.com>
2024-07-24 01:33:24 +0530

    making changes to deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@google/generative-ai'], // Add this line to externalize the module
+      external: ['@google/generative-ai'], // Externalize the module
     },
+  },
+  server: {
+    host: '0.0.0.0', // Ensure the server is accessible from outside
+    port: parseInt(import.meta.env.VITE_PORT, 10) || 4173, // Use VITE_PORT environment variable or default to 4173
   },
 });
