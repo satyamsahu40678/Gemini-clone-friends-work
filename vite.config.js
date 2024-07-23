6a5464b6b27603498667744b0bf985ce3aabdc1a
nerdnightboy <nerdnightboy@gmail.com>
2024-07-24 01:43:04 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,7 +9,8 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0', // Ensure the server is accessible from outside
-    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 4173, // Use PORT environment variable or default to 4173
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 4173,
   },
+  base: '/', // Ensure base path is correctly set
 });
