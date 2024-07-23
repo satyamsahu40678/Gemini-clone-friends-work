b89c3d44e51dffee9b1da3a13383467df3de1704
nerdnightboy <nerdnightboy@gmail.com>
2024-07-24 01:07:30 +0530

    gemini-clone of a friend

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@google/generative-ai'], // Add this line to externalize the module
+    },
+  },
+});
